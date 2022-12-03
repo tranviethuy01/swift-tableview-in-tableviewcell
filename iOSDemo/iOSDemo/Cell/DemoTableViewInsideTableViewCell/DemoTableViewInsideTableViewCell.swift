30ae3e5dac9384857a4ffa4e993a520049631f35
Viet Huy <tranviethuy01@gmail.com>
2022-12-03 21:03:21 +0700

    use dynamic cell

--- iOSDemo/iOSDemo/Cell/DemoTableViewInsideTableViewCell/DemoTableViewInsideTableViewCell.swift
+++ iOSDemo/iOSDemo/Cell/DemoTableViewInsideTableViewCell/DemoTableViewInsideTableViewCell.swift
@@ -9,10 +9,30 @@ import UIKit
 class DemoTableViewInsideTableViewCell: UITableViewCell {
     private var row: Int!
     @IBOutlet weak var contentLbl: UILabel!
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
         selectionStyle = .none
+        
+        setupUIView()
+    }
+    
+    private func setupUIView() {
+        
+        //setup tableView
+        let nib = UINib(nibName: SimpleTableViewFixedCellHeightCell.getCellIdentifier(), bundle: Bundle.main)
+        tableView.register(nib, forCellReuseIdentifier: SimpleTableViewFixedCellHeightCell.getCellIdentifier())
+        tableView.tableHeaderView = UIView.init(frame: CGRect.init(x: 0, y: 0, width: ViewService.screenSize().width, height: 0.1))
+        tableView.tableFooterView = UIView.init(frame: CGRect.init(x: 0, y: 0, width: ViewService.screenSize().width, height: 0.1))
+        tableView.sectionHeaderHeight = 0.1
+        tableView.sectionFooterHeight = 0.1
+        tableView.estimatedRowHeight = SimpleTableViewFixedCellHeightCell.getCellHeight()
+        tableView.rowHeight = UITableView.automaticDimension
+        tableView.delegate = self
+        tableView.dataSource = self
     }
     
     func setupData(row: Int){
@@ -38,5 +58,17 @@ class DemoTableViewInsideTableViewCell: UITableViewCell {
     class func getCellHeight() -> CGFloat {
         //just a test number, whether or not cell can auto resize
         return 200
+    }
+}
+
+extension DemoTableViewInsideTableViewCell: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: SimpleTableViewFixedCellHeightCell.getCellIdentifier(), for: indexPath) as! SimpleTableViewFixedCellHeightCell
+        cell.setupData(row: indexPath.row)
+        return cell
     }
 }
