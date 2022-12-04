3195c8c2430bba53bfd42b522e571822870d830c
Viet Huy <tranviethuy01@gmail.com>
2022-12-04 08:39:41 +0700

    add failure demo

---
+++ iOSDemo/iOSDemo/Cell/SimpleTableViewDynamicCellHeightCellFailConstraint/SimpleTableViewDynamicCellHeightCellFailConstraint.swift
@@ -0,0 +1,53 @@
+//
+//  SimpleTableViewDynamicCellHeightCellFailConstraint.swift
+//  iOSDemo
+//
+//  Created by Viet Huy on 12/4/22.
+//
+
+import UIKit
+/*note
+ What is wrong with this cell?
+ Constraint is wrong. We use align center to super view for a label.
+ So the system can not calculate the exact height of cell. 
+ */
+class SimpleTableViewDynamicCellHeightCellFailConstraint: UITableViewCell {
+    private var row: Int!
+    @IBOutlet weak var contentLbl: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        selectionStyle = .none
+    }
+    
+    func setupData(row: Int){
+        self.row = row
+        didSetupData()
+    }
+    
+    private func didSetupData(){
+        guard let row = self.row else {return}
+        var contentStr = "\(row). This is a simple demo cell with Dynamic height. Change additionalContentStr to see how auto Self-sizing Table View Cells work"
+        //this is a test string, change it to see change in cell and tableview with dynamic height constraint
+        var additionalContentStr = ""
+        //uncomment this to see change
+        additionalContentStr.append(contentsOf: "This is a simple demo cell with Dynamic height This is a simple demo cell with Dynamic height This is a simple demo cell with Dynamic height This is a simple demo cell with Dynamic height This is a simple demo cell with Dynamic height This is a simple demo cell with Dynamic height This is a simple demo cell with Dynamic height This is a simple demo cell with Dynamic height ")
+        contentStr = "\(contentStr) \(additionalContentStr)"
+        contentLbl.text = contentStr
+        if (row % 2 == 0) {
+            self.backgroundColor = .gray
+        } else {
+            self.backgroundColor = .darkGray
+        }
+        
+    }
+    
+    class func getCellIdentifier() -> String {
+        return "SimpleTableViewDynamicCellHeightCellFailConstraint"
+    }
+    
+    class func getCellHeight() -> CGFloat {
+        //just a test number, whether or not cell can auto resize
+        return 200
+    }
+}
